6db93aa461ced3e41bd02d08b4b538651bce207b
Katarzyna Jaroszewska <katarzyna_jaroszewska@wp.eu>
2020-07-05 21:53:00 +0200

    author_controller changes

--- src/main/java/com/kmj/springwebapp/controllers/AuthorController.java
+++ src/main/java/com/kmj/springwebapp/controllers/AuthorController.java
@@ -1,4 +1,23 @@
 package com.kmj.springwebapp.controllers;
 
+import com.kmj.springwebapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class AuthorController {
+
+    private final AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping(value = "/authors")
+    public String getAuthors(Model model) {
+        model.addAttribute("authors", authorRepository.findAll());
+
+        return "authors/list";
+    }
 }
